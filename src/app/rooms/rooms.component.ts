5f272d6d4c94352e2396a67a19d9417890f5d3eb
Samuel <samuel.muthaiga@student.moringaschool.com>
2023-08-14 01:49:55 +0300

    feat: create a RoomList interface to hold each rooms properties

--- src/app/rooms/rooms.component.ts
+++ src/app/rooms/rooms.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Room } from './rooms';
+import { Room, RoomList } from './rooms';
 
 @Component({
   selector: 'hinv-rooms',
@@ -13,12 +13,38 @@ export class RoomsComponent implements OnInit {
   hideRooms = false;
 
   rooms : Room = {
-
+    totalRooms: 20,
+    availableRooms: 10,
+    bookedRooms: 5
   }
 
-  // totalRooms: 20,
-  //   availableRooms: 10,
-  //   bookedRooms: 5
+  roomList : RoomList[] = [{
+    roomType : "Deluxe Room",
+    amenities : "Air conditioner, wifi, kitchen,bathroom,tv",
+    price : 1000,
+    photos : "https://rb.gy/t0hqa",
+    checkinTime : new Date("11-12-2022"),
+    checkoutTime : new Date("30-12-2022")
+  },
+  {
+    roomType : "Single Room",
+    amenities : "Air conditioner, wifi, kitchen,bathroom,tv",
+    price : 500,
+    photos : "https://rb.gy/t0hqa",
+    checkinTime : new Date("11-08-2022"),
+    checkoutTime : new Date("30-09-2022")
+  },
+  {
+    roomType : "Private Suite",
+    amenities : "Air conditioner, wifi, kitchen,bathroom,tv",
+    price : 1500,
+    photos : "https://rb.gy/t0hqa",
+    checkinTime : new Date("11-06-2022"),
+    checkoutTime : new Date("30-07-2022")
+  }
+]
+
+
   constructor () {}
   ngOnInit(): void {
   }
