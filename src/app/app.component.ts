7bf6b61762ebfe40d8d8e7f8072cd7c57a5bfbad
Samuel <samuel.muthaiga@student.moringaschool.com>
2023-08-13 14:19:51 +0300

    feat: use inline html templates. NB: this is not recommended. use a separate file to write your html code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,11 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  // templateUrl: './app.component.html',
+  // inline html muiltiline inline template
+  template: `<h1>Hello world from inline template</h1>
+  <p>Learning Angular is fun! Sober up!</p>
+  `,
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
