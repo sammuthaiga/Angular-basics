beeaaea1bfde90ec480af43910fda425b7fe8452
Samuel <samuel.muthaiga@student.moringaschool.com>
2023-08-14 02:32:18 +0300

    fix: rename the selector in the component decorator from app-root to hinc-root

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,18 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  // templateUrl: './app.component.html',
+  selector: 'hinv-root',
+   templateUrl: './app.component.html',
   // inline html muiltiline inline template
-  template: `<h1>Hello world from inline template</h1>
-  <p>Learning Angular is fun! Sober up!</p>
-  `,
-  styleUrls: ['./app.component.scss']
-})
+  // template: `<h1>Hello world from inline template</h1>
+  // <p>Learning Angular is fun! Sober up!</p>
+  // `,
+   styleUrls: ['./app.component.scss'],
+//   styles: [`h1 {
+//     color: Red;
+//     font-size: 128px;
+//     font-weight: bold;}`]
+ })
 export class AppComponent {
   title = 'hotelinventoryapp';
 }
