663c41a369dcd9cab10c723146021ce4cf2ceed5
Samuel <samuel.muthaiga@student.moringaschool.com>
2023-08-16 00:23:59 +0300

    feat:generate a rooms-list component. This component will handle the rooms list only and make it accessible form anywhere in the project

---
+++ src/app/rooms/rooms-list/rooms-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'hinv-rooms-list',
+  templateUrl: './rooms-list.component.html',
+  styleUrls: ['./rooms-list.component.scss']
+})
+export class RoomsListComponent {
+
+}
