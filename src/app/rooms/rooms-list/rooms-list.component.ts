1e87cc32ae943dae70ddc086d087e01749667e8c
Samuel <samuel.muthaiga@student.moringaschool.com>
2023-08-16 00:43:01 +0300

    fix: use @input tag to make rooms a valid html property of our project. data can now be passed into it

--- src/app/rooms/rooms-list/rooms-list.component.ts
+++ src/app/rooms/rooms-list/rooms-list.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { RoomList } from '../rooms';
 
 @Component({
   selector: 'hinv-rooms-list',
   templateUrl: './rooms-list.component.html',
   styleUrls: ['./rooms-list.component.scss']
 })
-export class RoomsListComponent {
+  export class RoomsListComponent  implements OnInit {
+
+    @Input() rooms: RoomList[] = [];
+
+  constructor () {}
+  ngOnInit(): void {
+
+  }
+
 
 }
