e03430acc3956098e4b8ff38dcb1815b78a51112
Samuel <samuel.muthaiga@student.moringaschool.com>
2023-08-17 23:33:54 +0300

    feat:implement the change detection strategy and select the .OnPush mode. N/B .OnPush mode is used when no data is to be modified internally. Otherwise result for default.

--- src/app/rooms/rooms-list/rooms-list.component.ts
+++ src/app/rooms/rooms-list/rooms-list.component.ts
@@ -1,11 +1,12 @@
 import { Room } from './../rooms';
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { ChangeDetectionStrategy, Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { RoomList } from '../rooms';
 
 @Component({
   selector: 'hinv-rooms-list',
   templateUrl: './rooms-list.component.html',
-  styleUrls: ['./rooms-list.component.scss']
+  styleUrls: ['./rooms-list.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
   export class RoomsListComponent  implements OnInit {
 
