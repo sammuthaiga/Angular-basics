c09261c83625f8ec1d32c6cf3dd5638ebf2dce44
Samuel <samuel.muthaiga@student.moringaschool.com>
2023-08-21 21:54:45 +0300

    enhance: utilize Input Binding for Title in Room List Component

--- src/app/rooms/rooms-list/rooms-list.component.ts
+++ src/app/rooms/rooms-list/rooms-list.component.ts
@@ -1,19 +1,26 @@
 import { Room } from './../rooms';
-import { ChangeDetectionStrategy, Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { ChangeDetectionStrategy, Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges } from '@angular/core';
 import { RoomList } from '../rooms';
 
 @Component({
   selector: 'hinv-rooms-list',
   templateUrl: './rooms-list.component.html',
   styleUrls: ['./rooms-list.component.scss'],
+  // implement change detection strategy in an .OnPush mode
+  // this ensures that that change is not triggered for this
+  // component unless needed.
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-  export class RoomsListComponent  implements OnInit {
+  export class RoomsListComponent  implements OnInit, OnChanges {
 
     @Input() rooms: RoomList[] = [];
+   
     @Output() selectedRoom = new EventEmitter<RoomList>();
 
   constructor () {}
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log(changes);
+  }
   ngOnInit(): void {
 
   }
