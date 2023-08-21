0bc53723c2baab3fcd78288d1cdd4c3e552ec629
Samuel <samuel.muthaiga@student.moringaschool.com>
2023-08-22 01:12:58 +0300

    feat: import and implement OnInIt and AfterContentInit lifecycle hooks

--- src/app/container/container.component.ts
+++ src/app/container/container.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { AfterContentInit, Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'hinv-container',
   templateUrl: './container.component.html',
   styleUrls: ['./container.component.scss']
 })
-export class ContainerComponent {
+export class ContainerComponent implements OnInit, AfterContentInit {
+
+  constructor () {}
+
 
 }
