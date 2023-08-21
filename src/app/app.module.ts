9c403048e7f045cb68bae5b4858f826fd9f53d03
Samuel <samuel.muthaiga@student.moringaschool.com>
2023-08-21 23:56:20 +0300

    feat: import the header component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RoomsComponent } from './rooms/rooms.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RoomsListComponent } from './rooms/rooms-list/rooms-list.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    RoomsComponent
+    RoomsComponent,
+    RoomsListComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
