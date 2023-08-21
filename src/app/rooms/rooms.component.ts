144deb6100d93be91be259208abc4d1572c67f9f
Samuel <samuel.muthaiga@student.moringaschool.com>
2023-08-22 02:09:18 +0300

    feat: access the roomlist through the rooms service

--- src/app/rooms/rooms.component.ts
+++ src/app/rooms/rooms.component.ts
@@ -1,6 +1,7 @@
 import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
 import { Room, RoomList } from './rooms';
 import { HeaderComponent } from '../header/header.component';
+import { RoomsService } from './services/rooms.service';
 
 @Component({
   selector: 'hinv-rooms',
@@ -25,42 +26,13 @@ export class RoomsComponent implements OnInit, AfterViewInit {
 
   roomList : RoomList[] = [];
   @ViewChild(HeaderComponent) headercomponent!: HeaderComponent;
-  constructor () {}
+  
+  constructor (private roomsService : RoomsService) {}
 
   ngOnInit(): void {
     // console.log(this.headercomponent)
-    this.roomList = [
-      {
-        roomNumber : 1,
-        roomType : "Deluxe Room",
-        amenities : "Air conditioner, wifi, kitchen,bathroom,tv",
-        price : 1000,
-        photos : "https://rb.gy/t0hqa",
-        checkinTime : new Date("11-12-2022"),
-        checkoutTime : new Date("30-12-2022"),
-        rating: 2.8
-      },
-      {
-        roomNumber : 2,
-        roomType : "Single Room",
-        amenities : "Air conditioner, wifi, kitchen,bathroom,tv",
-        price : 500,
-        photos : "https://rb.gy/t0hqa",
-        checkinTime : new Date("11-08-2022"),
-        checkoutTime : new Date("30-09-2022"),
-        rating: 3.5
-      },
-      {
-        roomNumber : 3,
-        roomType : "Private Suite",
-        amenities : "Air conditioner, wifi, kitchen,bathroom,tv",
-        price : 1500,
-        photos : "https://rb.gy/t0hqa",
-        checkinTime : new Date("11-06-2022"),
-        checkoutTime : new Date("30-07-2022"),
-        rating: 4.5
-      }
-    ]
+    this.roomList = this.roomsService.getRooms()
+
   }
 
   ngAfterViewInit(): void {
--- src/app/rooms/services/rooms.service.ts
+++ src/app/rooms/services/rooms.service.ts
@@ -1,9 +1,46 @@
 import { Injectable } from '@angular/core';
+import { RoomList } from '../rooms';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RoomsService {
+  roomList : RoomList[] = [
+    {
+      roomNumber : 1,
+      roomType : "Deluxe Room",
+      amenities : "Air conditioner, wifi, kitchen,bathroom,tv",
+      price : 1000,
+      photos : "https://rb.gy/t0hqa",
+      checkinTime : new Date("11-12-2022"),
+      checkoutTime : new Date("30-12-2022"),
+      rating: 2.8
+    },
+    {
+      roomNumber : 2,
+      roomType : "Single Room",
+      amenities : "Air conditioner, wifi, kitchen,bathroom,tv",
+      price : 500,
+      photos : "https://rb.gy/t0hqa",
+      checkinTime : new Date("11-08-2022"),
+      checkoutTime : new Date("30-09-2022"),
+      rating: 3.5
+    },
+    {
+      roomNumber : 3,
+      roomType : "Private Suite",
+      amenities : "Air conditioner, wifi, kitchen,bathroom,tv",
+      price : 1500,
+      photos : "https://rb.gy/t0hqa",
+      checkinTime : new Date("11-06-2022"),
+      checkoutTime : new Date("30-07-2022"),
+      rating: 4.5
+    }
+  ]
 
   constructor() { }
+
+  getRooms () {
+    return this.roomList;
+  }
 }
